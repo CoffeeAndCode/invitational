edb160593001b0080cd58e625370a77c4959c60b
Jonathan Knapp <jon@coffeeandcode.com>
2019-09-26 10:22:28 -0400

    Allow invitable to be nil for uberadmins

--- app/modules/invitational/invitation_core.rb
+++ app/modules/invitational/invitation_core.rb
@@ -3,7 +3,7 @@ module Invitational
     extend ActiveSupport::Concern
 
     included do
-      belongs_to :invitable, :polymorphic => true
+      belongs_to :invitable, :optional => true, :polymorphic => true
 
       before_create :setup_hash
 
